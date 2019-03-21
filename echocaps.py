1c91d807df56377eb3f56c0cd25237787d257072
abelantunes98 <abel.neto@ccc.ufcg.edu.br>
2019-03-21 01:11:37 -0300

    socketCaps

---
+++ echocaps.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+import socket
+import sys
+
+porta = int(sys.argv[1] if len(sys.argv) > 1 else 3100)
+
+with socket.socket() as s:
+    # Abrindo porta
+    s.bind(('localhost', porta))
+    # Escutando na porta
+    s.listen()
+
+    print ("Aguardando conexao na porta %s..." % porta)
+    
+    # Aceitando conexoes
+    conexao, endereco = s.accept()
+
+    # Depois de conectado
+    with conexao:
+        print ("Conexao estabelecida de %s:%s..." % endereco)
+        mensagem = "EchoCaps"
+
+        # Para ao receber uma mensagem vazia
+        while (mensagem != '\\n' and mensagem):
+            print ("Aguardando mensagem de %s:%s" % endereco)
+            mensagem = conexao.recv(4096)
+            print("Mensagem: %s" % mensagem)
+            # Devolvendo mensagem com uppercase
+            conexao.sendall(mensagem.upper())
+        
+        conexao.close()
+
+
+        
+    
+
