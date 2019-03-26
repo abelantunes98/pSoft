e63a94350361bdf925c5c775757301dc08a429c3
abelantunes98 <abel.neto@ccc.ufcg.edu.br>
2019-03-26 06:12:26 -0300

    chat2

--- chat/chatServer.py
+++ chat/chatServer.py
@@ -3,48 +3,59 @@
 from threading import Thread
 import socket
 import sys
+import os
 
-porta = int(sys.argv[1] if len(sys.argv) > 1 else 3100)
-users = {}
+porta = int(sys.argv[1] if(len(sys.argv) > 1) else 3103)
+usuarios = {}
 mensagens = []
 
-def iniciaCliente(endereco, users):
+def iniciaCliente(endereco, usuarios, mensagens):
     
-    conexao = users[endereco]
+    conexao = usuarios[endereco]
     with conexao as sk:
-        while True:
+        
+        mensagemSv = ""
+        while mensagemSv != ":bye":
             mensagem = sk.recv(4096)
-            if mensagem == "bye":
-                break
+            mensagemSv = mensagem.decode('utf-8').split(os.linesep)[0]
 
-            mensagens.append(mensagem)
+            if (mensagemSv != ":bye"):
+                # Adicionando a identificacao de quem está mandando a mensagem.
+                saida = (str(endereco) + " " +  mensagemSv + os.linesep).encode('utf-8')
+                mensagens.append((endereco ,saida))
+           
+            else:
+                del usuarios[endereco]
+                saida = str(endereco) + " saiu do grupo!" + os.linesep
+                # Transforma em obj que pode ser enviado
+                mensagens.append((endereco, saida.encode('utf-8')))
 
+
+def receberClientes(usuarios, mensagens):
+        
+    while True:        
+        # Escutando na porta
+        s.listen()
+
+        # Aceitando conexoes
+        conexao, endereco = s.accept()
+        usuarios[endereco] = conexao
+        Thread(target=iniciaCliente, args=(endereco, usuarios, mensagens)).start() 
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Abrindo porta
     s.bind(('localhost', porta))
-    # Escutando na porta
-    s.listen()
-
-    print ("Aguardando conexao na porta %s..." % porta)
+    Thread(target=receberClientes, args=(usuarios, mensagens)).start()
     
-    # Aceitando conexoes
-    conexao, endereco = s.accept()
-    users[endereco] = conexao
-    Thread(target=iniciaCliente, args=(endereco, users)).start()
+    while True:
 
-
-while True:
-    while len(mensagens) > 0:
-        for cli in users.values():
-            cli.sendall(mensagens[0])
-
-        mensagens.pop(0)
+        while len (mensagens) > 0:
+            for cli in usuarios.values():
                 
-
-
-
-        
+                # Olha se o usuario nao e o que mandou a mensagem
+                if (mensagens[0][0] not in usuarios.keys()) or (cli != usuarios[mensagens[0][0]]):
+                    cli.send(mensagens[0][1])
+            mensagens.pop(0)
     
 
